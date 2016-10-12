b2285e95461967aebb298469eb5335f7152aa5f8
Luca Trubbiani <luca.trubbiani@gmail.com>
2016-10-12 10:11:54 +0200

    Update dfa.js

--- dfa.js
+++ dfa.js
@@ -1,4 +1,4 @@
-Set.prototype.isSuperset = function(subset) {
+Set.prototype.isSuperset = function (subset) {
     for (var elem of subset) {
         if (!this.has(elem)) {
             return false;
@@ -7,7 +7,7 @@ Set.prototype.isSuperset = function(subset) {
     return true;
 }
 
-Set.prototype.union = function(setB) {
+Set.prototype.union = function (setB) {
     var union = new Set(this);
     for (var elem of setB) {
         union.add(elem);
@@ -15,7 +15,7 @@ Set.prototype.union = function(setB) {
     return union;
 }
 
-Set.prototype.intersection = function(setB) {
+Set.prototype.intersection = function (setB) {
     var intersection = new Set();
     for (var elem of setB) {
         if (this.has(elem)) {
@@ -25,7 +25,7 @@ Set.prototype.intersection = function(setB) {
     return intersection;
 }
 
-Set.prototype.difference = function(setB) {
+Set.prototype.difference = function (setB) {
     var difference = new Set(this);
     for (var elem of setB) {
         difference.delete(elem);
@@ -34,59 +34,58 @@ Set.prototype.difference = function(setB) {
 }
 
 
-var DFA = function(transition, final, start) {
-  this.transition = transition;
-  this.final = final;
-  this.start = start
+var DFA = function (transition, final, start) {
+    this.transition = transition;
+    this.final = final;
+    this.start = start
 };
 
-DFA.prototype.states = function (){
-			var states = new Set();
-      for (var key in this.transition) {
-      if (this.transition.hasOwnProperty(key)) {
-       if (!states.has(key))
-					{
-						states.add(parseInt(key));
-				}
-      }
-		}
-		return states;
-		};
-    
-DFA.prototype.alphabet = function (){
-			var alphabet = new Set();
-      
-      for (var key in this.transition) {
-      if (this.transition.hasOwnProperty(key)) {
-       for (var symbol in this.transition[key])
-				{
-					if (!alphabet.has(symbol))
-					{
-						alphabet.add(symbol);
-					}
-				}
-      }
-		}
-		return alphabet;
+DFA.prototype.states = function () {
+    var states = new Set();
+    for (var key in this.transition) {
+        if (this.transition.hasOwnProperty(key)) {
+            if (!states.has(key)) {
+                states.add(parseInt(key));
+            }
+        }
+    }
+    return states;
+};
+
+DFA.prototype.alphabet = function () {
+    var alphabet = new Set();
+
+    for (var key in this.transition) {
+        if (this.transition.hasOwnProperty(key)) {
+            for (var symbol in this.transition[key]) {
+                if (!alphabet.has(symbol)) {
+                    alphabet.add(symbol);
+                }
+            }
+        }
+    }
+    return alphabet;
 };
 DFA.prototype.test = function (string) {
-			var state = this.start,
-			index = 0;
-			var length = string.length;
-			var transition = this.transition;
-			while (index < length) {
-      	state = transition[state][string.charAt(index++)];
-        
-				if (typeof state === "undefined")
-					return false;
-			}
+    var state = this.start,
+        index = 0;
+    var length = string.length;
+    var transition = this.transition;
+    while (index < length) {
+        state = transition[state][string.charAt(index++)];
 
-			return this.final.indexOf(state) >= 0;
-		};
+        if (typeof state === "undefined")
+            return false;
+    }
+
+    return this.final.indexOf(state) >= 0;
+};
 /*
-let reachable_states:= {q0};
-let new_states:= {q0};
-do {
+ * Unreachable states
+ 
+ let reachable_states:= {q0};
+ let new_states:= {q0};
+ do {
     temp := the empty set;
     for each q in new_states do
         for all c in Σ do
@@ -95,39 +94,40 @@ do {
     end;
     new_states := temp \ reachable_states;
     reachable_states := reachable_states ∪ new_states;
-} while(new_states ≠ the empty set);
-unreachable_states := Q \ reachable_states;
-*/
+ } while(new_states ≠ the empty set);
+ unreachable_states := Q \ reachable_states;
+ *
+ */
 
-DFA.prototype.unreachable = function(){
-	var reachable_states = new Set().add(this.start);
-  var new_states = new Set().add(this.start);
-  var transition = this.transition;
-  var alp = this.alphabet();
-  do{
-    var temp = new Set();
+DFA.prototype.unreachable = function () {
+    var reachable_states = new Set().add(this.start);
+    var new_states = new Set().add(this.start);
+    var transition = this.transition;
+    var alp = this.alphabet();
+    do {
+        var temp = new Set();
 
-    new_states.forEach(function (q) {
-      alp.forEach(function(c){
-          var tstate = transition[q][c];
-          if(tstate !== "undefined")
-              temp.add(tstate);
-      });
-    });
-    new_states = temp.difference(reachable_states);
-    reachable_states = reachable_states.union(new_states);
-  }while(new_states.size != 0);
-  var unreachable_states = dfa.states().difference(reachable_states);
-  return unreachable_states;
+        new_states.forEach(function (q) {
+            alp.forEach(function (c) {
+                var tstate = transition[q][c];
+                if (tstate !== "undefined")
+                    temp.add(tstate);
+            });
+        });
+        new_states = temp.difference(reachable_states);
+        reachable_states = reachable_states.union(new_states);
+    } while (new_states.size != 0);
+    var unreachable_states = dfa.states().difference(reachable_states);
+    return unreachable_states;
 };
 
 /*
  * Hopcroft's algorithm - wikipedia
  * https://en.wikipedia.org/wiki/DFA_minimization
-
-P := {F, Q \ F};
-W := {F};
-while (W is not empty) do
+ 
+ P := {F, Q \ F};
+ W := {F};
+ while (W is not empty) do
      choose and remove a set A from W
      for each c in Σ do
           let X be the set of states for which a transition on c leads to a state in A
@@ -142,26 +142,27 @@ while (W is not empty) do
                          add Y \ X to W
           end;
      end;
-end;
-*/
-DFA.prototype.minimize = function(){
-	// TODO 
+ end;
+ */
+
+DFA.prototype.minimize = function () {
+    // TODO 
 }
 
-var dfa = new DFA({1: {"a":1,"b":2}, 2:{"a":1,"b":2},3:{"a":1,"b":2}}, [1], 1);
+var dfa = new DFA({1: {"a": 1, "b": 2}, 2: {"a": 1, "b": 2}, 3: {"a": 1, "b": 2}}, [1], 1);
 //console.log(dfa.alphabet());
 //console.log(dfa.test("abbba"));
 console.log(dfa.unreachable());
 
 /**
-TEST operation
-var setA = new Set([1,2,3,4]),
-    setB = new Set([2,3]),
-    setC = new Set([3,4,5,6]);
+ TEST operation
+ var setA = new Set([1,2,3,4]),
+ setB = new Set([2,3]),
+ setC = new Set([3,4,5,6]);
 
-setA.isSuperset(setB); // => true
-setA.union(setC); // => Set [1, 2, 3, 4, 5, 6]
-setA.intersection(setC); // => Set [3, 4]
-setA.difference(setC); // => Set [1, 2]
-console.log(setA);
-**/
+ setA.isSuperset(setB); // => true
+ setA.union(setC); // => Set [1, 2, 3, 4, 5, 6]
+ setA.intersection(setC); // => Set [3, 4]
+ setA.difference(setC); // => Set [1, 2]
+ console.log(setA);
+ **/
