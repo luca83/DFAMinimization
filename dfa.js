720a8fcd97b566c3d6bab11d7f600ad621ed2b9b
Luca Trubbiani <luca.trubbiani@gmail.com>
2016-10-12 10:09:04 +0200

    Update dfa.js

--- dfa.js
+++ dfa.js
@@ -99,15 +99,16 @@ do {
 unreachable_states := Q \ reachable_states;
 */
 
-DFA.prototype.unreachable = function(dfa){
+DFA.prototype.unreachable = function(){
 	var reachable_states = new Set().add(this.start);
   var new_states = new Set().add(this.start);
   var transition = this.transition;
+  var alp = this.alphabet();
   do{
     var temp = new Set();
 
     new_states.forEach(function (q) {
-      dfa.alphabet().forEach(function(c){
+      alp.forEach(function(c){
           var tstate = transition[q][c];
           if(tstate !== "undefined")
               temp.add(tstate);
@@ -118,13 +119,39 @@ DFA.prototype.unreachable = function(dfa){
   }while(new_states.size != 0);
   var unreachable_states = dfa.states().difference(reachable_states);
   return unreachable_states;
-}
+};
 
+/*
+ * Hopcroft's algorithm - wikipedia
+ * https://en.wikipedia.org/wiki/DFA_minimization
+
+P := {F, Q \ F};
+W := {F};
+while (W is not empty) do
+     choose and remove a set A from W
+     for each c in Σ do
+          let X be the set of states for which a transition on c leads to a state in A
+          for each set Y in P for which X ∩ Y is nonempty and Y \ X is nonempty do
+               replace Y in P by the two sets X ∩ Y and Y \ X
+               if Y is in W
+                    replace Y in W by the same two sets
+               else
+                    if |X ∩ Y| <= |Y \ X|
+                         add X ∩ Y to W
+                    else
+                         add Y \ X to W
+          end;
+     end;
+end;
+*/
+DFA.prototype.minimize = function(){
+	// TODO 
+}
 
 var dfa = new DFA({1: {"a":1,"b":2}, 2:{"a":1,"b":2},3:{"a":1,"b":2}}, [1], 1);
 //console.log(dfa.alphabet());
 //console.log(dfa.test("abbba"));
-console.log(dfa.unreachable(dfa));
+console.log(dfa.unreachable());
 
 /**
 TEST operation
